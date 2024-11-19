60ddafbf096d5275c968f8d8df22081e3d145a14
gohar_ter_stepanyan <gohar9970@gmail.com>
2024-11-19 22:00:27 +0400

    learn for

--- 19.11/ident.js
+++ 19.11/ident.js
@@ -1,1 +1,15 @@
-console.log(10)
+//let
+//const
+//var
+let i = 0;
+for (; i < 5; i++) {
+    console.log(i);
+}
+console.log(i, 'in global value');
+
+
+var x = 20;
+if (true)   {
+    var x = 10;
+    console.log(x);
+}
